6599afa27f9adeb26825dbc4823b901fc6f650b4
ZLP00 <1186121106@qq.com>
2021-01-30 21:20:54 +0800

    nacos 使用命名空间 并支持多配置文件

--- config/src/main/java/com/example/demo/controller/AppController.java
+++ config/src/main/java/com/example/demo/controller/AppController.java
@@ -9,20 +9,32 @@ import org.springframework.web.bind.annotation.RestController;
  * 程序员  by dell
  * time  2021-01-30
  **/
-
 @RestController
-@RefreshScope
+@RefreshScope// 动态刷新
 public class AppController {
 
     @Value("${animal.name}")
     private String name;
 
+    @Value("${animal.age}")
+    private int age;
+
+
+    @Value("${person.name}")
+    private String personName;
+
+    @Value("${person.age}")
+    private int personAge;
 
     @RequestMapping(value = "/config")
     public String config(){
-
         System.out.println("执行配置");
-        return name;
+        return name+":"+age;
+    }
 
+    @RequestMapping(value = "/personConfig")
+    public String config2(){
+        System.out.println("执行配置");
+        return personName+":"+personAge;
     }
 }
